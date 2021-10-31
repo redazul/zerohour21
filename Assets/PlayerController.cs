7afffdb80bae7c198d0cf8e3a4b04614dc9ef111
BeastBomber23 <beastbomber23@gmail.com>
2021-10-30 22:03:55 -0400

    Optimization and Menu Fix

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,53 +12,14 @@ public class PlayerController : MonoBehaviour
     public GameObject Cylinder;
     public bool cylinderOn = true;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
-        if(Input.GetKeyDown("q"))
-        {
-            if(capsuleOn)
-            {
-            capsuleOn=false;
-            Capsule.SetActive(false);
-            }else{
-                    Capsule.SetActive(true);
-                    capsuleOn=true;
-            }
-        }
+        if(Input.GetKeyDown("q")) Capsule.SetActive(!Capsule.active);
 
-        if(Input.GetKeyDown("w"))
-        {
-                        if(cubeOn)
-            {
-            
-            cubeOn=false;
-            Cube.SetActive(false);
-            }else{
-                    Cube.SetActive(true);
-                     cubeOn=true;
-            }
-        }
+        if(Input.GetKeyDown("w")) Cube.SetActive(!Cube.active);
 
-        if(Input.GetKeyDown("e"))
-        {
-                       if(cylinderOn)
-            {
-            cylinderOn=false;
-            Cylinder.SetActive(false);
-            }else{
-                    Cylinder.SetActive(true);
-                     cylinderOn=true;
-            };
-        }
+        if(Input.GetKeyDown("e")) Cylinder.SetActive(!Cylinder.active);
         
     }
 }
